c81017be188f4134c684869b7d225a5332fcf197
Abdullah Ayaz <atillawall@gmail.com>
2022-04-29 23:11:01 +0500

    redux_added

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "App";
+import { store } from "redux/store";
+import { Provider } from "react-redux";
 
 // Soft UI Context Provider
 import { SoftUIControllerProvider } from "context";
 
 ReactDOM.render(
   <BrowserRouter>
-    <SoftUIControllerProvider>
-      <App />
-    </SoftUIControllerProvider>
+    <Provider store={store}>
+      <SoftUIControllerProvider>
+        <App />
+      </SoftUIControllerProvider>
+    </Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
